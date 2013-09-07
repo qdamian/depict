9a6785eb5c8824c2143a78da47ee4b9a80e14624
Damian Quiroga <qdamian@gmail.com>
2013-09-07 16:36:37 -0300

    Adding pylint to CI

--- pre-push.sh
+++ pre-push.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 
 set -e
+
+# Install
 pip install -r python/requirements --use-mirrors
 npm install
+
+# Test
+pylint --rcfile=python/src/.pylintrc python/src/depict/
 nosetests python
 npm test
