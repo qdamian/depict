ab2f84a6f40599a4802d6f3c7ba47812c9c476b4
Damian Quiroga <qdamian@gmail.com>
2013-09-22 04:18:07 -0300

    Adding basic usage message

---
+++ features/help.feature
@@ -0,0 +1,24 @@
+# Copyright 2013 Damian Quiroga
+#
+# This file is part of Depict.
+#
+# Depict is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Depict is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Depict.  If not, see <http://www.gnu.org/licenses/>.
+
+Feature: help
+
+# User story: As a first-time user I want the tool to guide me on how to use it.
+
+Scenario:
+    When I run depict with incorrect options
+    Then I see basic usage information
---
+++ python/src/depict/__main__.py
@@ -0,0 +1,26 @@
+# Copyright 2013 Damian Quiroga
+#
+# This file is part of Depict.
+#
+# Depict is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Depict is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Depict.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+
+# pylint:disable = invalid-name
+
+desc = 'Create representations of a python program'
+parser = argparse.ArgumentParser(prog='python -m depict',
+                                 description=desc)
+args = parser.parse_args()
+parser.print_help()
