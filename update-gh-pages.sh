542306066008054ab024cafb743f351d8f407e02
Damian Quiroga <qdamian@gmail.com>
2013-10-17 01:00:28 -0300

    Testing script to update gh-pages on build success

---
+++ update-gh-pages.sh
@@ -0,0 +1,20 @@
+set -e
+
+if [[ -z "$TRAVIS_BRANCH" ]]; then
+    exit 1
+
+git config --global user.email "qdamian@gmail.com"
+git config --global user.name "Travis"
+
+cd $HOME
+git clone --branch=gh-pages https://${GH_TOKEN}@github.com/qdamian/depict gh-pages > /dev/null
+echo clone: $?
+
+cd gh-pages
+date > now
+git add now
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER, updating gh-pages" > /dev/null
+echo commit: $?
+
+git push origin gh-pages
+echo push: $?
