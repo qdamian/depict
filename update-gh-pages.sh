3368509a5b2b57415203bdf3beae589acc7acef6
Damian Quiroga <qdamian@gmail.com>
2014-01-24 21:02:09 -0300

    Logging steps to publish gh-pages

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -7,11 +7,13 @@ function silently_quit_if_not_in_travis {
 }
 
 function setup_git {
+    echo Setting git user
     git config --global user.email "qdamian@gmail.com"
     git config --global user.name "Travis"
 }
 
 function clone_gh_pages {
+    echo Cloning gh-pages
     cd $HOME
     ! test -d gh-pages || rm -rf gh-pages
     git clone --branch=gh-pages https://${GH_TOKEN}@github.com/qdamian/depict gh-pages
@@ -19,6 +21,7 @@ function clone_gh_pages {
 }
 
 function update_html5_files {
+    echo Updating HTML5 files
     ! test -d test || git rm -f demo
     git checkout origin/HEAD -- html5
     mv html5 demo
@@ -26,6 +29,7 @@ function update_html5_files {
 }
 
 function publish_changes {
+    echo Publishing changes
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER, updating gh-pages"
     git push origin gh-pages
 }
